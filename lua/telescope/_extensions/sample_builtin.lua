d42a1c507f3fe11452bb60457c58b2e15dd537df
lunarxlark <lunarxlark@gmail.com>
2022-06-06 22:09:27 +0900

    first picker

---
+++ lua/telescope/_extensions/sample_builtin.lua
@@ -0,0 +1,17 @@
+local pickers = require "telescope.pickers" -- pickerを作るために使う主なモジュール
+local finders = require "telescope.finders" -- itemでpickerを一覧表示するためのインタフェース
+local conf = require("telescope.config").values -- valuesテーブルはユーザの設定を保持する。
+
+
+local colors = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "colors",
+    finder = finders.new_table {
+      results = {"red", "green", "blue"}
+    },
+    sorter = conf.generic_sorter(opts),
+  }):find()
+end
+
+colors()
